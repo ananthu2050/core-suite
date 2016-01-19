cac6d33f5a573e2648c21fe661fcd24694b1861f
geraldarthur <gerald.a.rich@gmail.com>
2016-01-18 18:18:06 -0800

    update before merge

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ The following key/value pairs need to be returned by a test:
 ```
 var result = {
   passed: false,  // whether or not the test passed
-  message: "You're missing a column header", // descriptive text about this test: 
-  template: _.template(`<span class="test-header">Missing column headers: <%= missingHeaders %></span>`)({ missingHeaders: 42}) // descriptive text which will dynamically display the results of the test
+  message: "You're missing a column header", // descriptive text about this test rendered to the CLI
+  template: _.template(`<span class="test-header">Missing column headers: <%= missingHeaders %></span>`)({ missingHeaders: 42}) // descriptive text which will dynamically display the results of the test in the desktop app
 };
 ```
--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function columnsContainNothing(rows) {
   var nothing = {};
   cols.forEach(function(col) {
     nothing[col] = 0;
-  })
+  });
   // look through the rows
   rows.forEach(function(row) {
     cols.forEach(function(col) {
@@ -162,30 +162,37 @@ exports.tests.push(columnsContainNumbers)
  */
 function checkColumnHeaders(rows, str) {
   console.log("checking column headers", rows.length);
-
-  var firstRow = jsonData[0];
-  var columnHeadersArray = _.keys(firstRow);
-  var totalColumnsCount = columnHeadersArray.length;
+  var columnHeads = Object.keys(rows[0]);
+  var noHeader = {};
+  columnHeads.forEach(function(columnHead, index) {
+    noHeader[index] = columnHead;
+  });
+  var totalColumnsCount = columnHeads.length;
   var missingHeadersCount;
+  var isPassed;
 
-  for (var i = 0; i < columnHeadersArray.length; i++) {
-    var currItem = columnHeadersArray[i];
+  for (var i = 0; i < columnHeads.length; i++) {
+    var currentItem = columnHeads[i];
     if (currItem.length === 0) {
       missingHeadersCount += 1;
     }
   }
 
   if (missingHeadersCount > 0) {
-    testResult.passed = false;
+    isPassed = false;
   } else {
-    testResult.passed = true;
+    isPassed = true;
   }
 
-  var message;
-  var template;
+  var message = "Columns without headers:<br/> ";
+  columnHeads.forEach(function(columnHead, index) {
+    message += "column " + index
+    if(i < columnHeads.length-1) message += "<br/> "
+  })
+  var template = ;
 
   var result = {
-    passed: null,
+    passed: isPassed,
     message: message,
     template: template
   };
