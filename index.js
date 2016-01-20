99e3410810e129f7d4ca1a39a1de2a4da3397d6e
Ian Johnson <enjalot@gmail.com>
2016-01-19 16:17:43 -0800

    test for strings of length 255. closes https://github.com/dataproofer/Dataproofer/issues/6

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function fooBar(rows, str, input) {
 function numberOfRows(rows) {
   var message = "This spreadsheet has " + rows.length + " rows"
   var template = _.template(`
-    <span class="test-header">This spreadsheet has <%= rows %> rows</span>
+    <span>This spreadsheet has <%= rows %> rows</span>
   `)({ rows: rows.length })
   var result = {
     passed: true, // this doesn't really fail, as it is mostly an insight
@@ -67,10 +67,10 @@ exports.tests.push(numberOfRows)
  */
 function numberOfRowsIs65k(rows) {
   var message, template, passed;
-  if(rows.length === 65536) {
+  if(rows.length === 65536 || rows.length === 65535) { // including both for now, not clear if header row should be included
     message = "Warning: This spreadsheet has " + rows.length + " rows, a common cutoff point for Excel indicating your dataset may be missing rows."
     template = _.template(`
-      <span class="test-header warning">This spreadsheet has <%= rows %> rows, a common cutoff point for Excel indicating your dataset may be missing rows.</span>
+      <span class="warning">This spreadsheet has <%= rows %> rows, a common cutoff point for Excel indicating your dataset may be missing rows.</span>
     `)({ rows: rows.length })
     passed = false;
   } else {
@@ -200,6 +200,69 @@ function columnsContainNumbers(rows) {
   return result;
 }
 exports.tests.push(columnsContainNumbers)
+
+
+/**
+ * Determine the cells that have exactly 255 characters (SQL upper limit error)
+ * @param  {Array} the rows of the spreadsheet
+ * @return {Object} the result
+ */
+function stringsHaveExactly255Characters(rows) {
+  var cols = Object.keys(rows[0]);
+  var strings = {};
+  cols.forEach(function(col) {
+    strings[col] = 0;
+  })
+  var cells = [] // we will want to mark cells to be highlighted here
+  var has255 = false
+  // look through the rows
+  rows.forEach(function(row) {
+    var crow = {} // we make a row to keep track of cells we want to highlight
+    cols.forEach(function(col) {
+      var cell = row[col];
+      if(cell.length === 255) {
+        crow[col] = 1
+        strings[col] += 1
+        has255 = true; // we want to know if it occurrs at least once
+      } else {
+        crow[col] = 0
+      }
+    })
+    cells.push(crow) // push our marking row onto our cells array
+  })
+
+  var message, template, passed;
+  if(has255) {
+    passed = false
+    message = "Warning: we found strings that are exactly 255 characters long. This can indicate an error with a SQL export: ";
+    cols.forEach(function(col, i) {
+      message += col + ": " + strings[col]
+      if(i < cols.length-1) message += "<br/> "
+    })
+
+    template = _.template(`Warning: we found strings that are exactly 255 characters long. This can indicate an error with a SQL export:<br/>
+    <% _.forEach(cols, function(col) { %>
+      <% if(strings[col]) { %>
+      We found <span class="test-value"><%= strings[col] %></span> cells (<%= percent(strings[col]/rows.length) %>) with 255 character-length strings for column <span class="test-column"><%= col %></span><br/>
+      <% } %>
+    <% }) %>
+    `)({ cols: cols, strings: strings, rows: rows, percent: percent })
+
+  } else {
+    passed = true;
+    message = "No anomolies found with character lengths of cells";
+  }
+
+  var result = {
+    passed: true, // this doesn't really fail, as it is mostly an insight
+    title: "Strings with 255 Characters (SQL upper limit error)",
+    highlightCells: cells, // a mirror of the dataset, but with a 1 or 0 for each cell if it should be highlighted or not
+    message: message,
+    template: template
+  }
+  return result;
+}
+exports.tests.push(stringsHaveExactly255Characters)
 
 
 /** 
