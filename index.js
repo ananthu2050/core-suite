bd1085f940f91854af4b812354ee896d851e9e86
geraldarthur <gerald.a.rich@gmail.com>
2016-04-01 12:06:32 -0700

    update index.js require statements

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ exports = module.exports = {
   name: "dataproofer-core-suite",
   fullName: "Core Data Tests",
   tests: [],      // the list of main tests to be run in the suite
-  subtests: [],   // a list of tests that can be triggered by the main tests but wont be run automatically
-}
+  subtests: []   // a list of tests that can be triggered by the main tests but wont be run automatically
+};
 
-var checkDuplicateRows = require('./src/checkDuplicateRows');
-var numberOfRowsIs65k = require('./src/numberOfRowsIs65k');
-var stringsHaveExactly255Characters = require('./src/stringsHaveExactly255Characters');
-var maxSummedInteger = require('./src/maxSummedInteger');
-var maxInteger = require('./src/maxInteger');
-var maxSmallInteger = require('./src/maxSmallInteger');
+var checkDuplicateRows = require("./src/checkDuplicateRows");
+var maxInteger = require("./src/maxInteger");
+var maxSmallInteger = require("./src/maxSmallInteger");
+var maxBigInteger = require("./src/maxBigInteger");
+var maxSummedInteger = require("./src/maxSummedInteger");
+var numberOfRowsIs65k = require("./src/numberOfRowsIs65k");
+var stringsHaveExactly255Characters = require("./src/stringsHaveExactly255Characters");
 
 exports.tests.push(
-  numberOfRowsIs65k,
   checkDuplicateRows,
+  numberOfRowsIs65k,
   stringsHaveExactly255Characters,
-  maxSummedInteger,
   maxInteger,
-  maxSmallInteger
+  maxSummedInteger,
+  maxSmallInteger,
+  maxBigInteger
 );
